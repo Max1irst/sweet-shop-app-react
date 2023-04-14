77797ef678d4f0634681ddc4d1a571f53327cb55
Maksim <maksim.bobrov@nuacom.net>
2023-04-14 15:29:21 +0300

    first page done

--- src/components/reviews/reviews-block.jsx
+++ src/components/reviews/reviews-block.jsx
@@ -2,7 +2,9 @@ import './reviews-block.scss';
 import profileImg from '../../images/img-logo.jpg';
 import star from '../../images/star.png';
 import ReviewItem from './review-item';
-import user1 from '../../images/user3.png';
+import user1 from '../../images/user1.png';
+import user2 from '../../images/user2.png';
+import user3 from '../../images/user3.png';
 function ReviewsBlock() {
   return (
     <div className="reviews-block">
@@ -32,11 +34,22 @@ function ReviewsBlock() {
       </div>
       <div className="reviews-list">
         <ReviewItem
-          userImg={user1}
+          userImg={user3}
           userName={'Katrin B'}
-          date="April 11 2023"
-          comment="Good product and fast delivery
-Good product and fast delivery, East to order and great choice.I will be ordering again."
+          date="April 11, 2023"
+          comment="Good product and fast delivery, East to order and great choice.I will be ordering again."
+        />
+        <ReviewItem
+          userImg={user2}
+          userName="Donna Reid"
+          date="April 9, 2023"
+          comment="Excellent choice of sweets and crisps, fast delivery, my girls love the range and can't wait for their next delivery, highly recommend, will definitely be ordering again."
+        />
+        <ReviewItem
+          userImg={user1}
+          userName="lorraine"
+          date="April 10, 2023"
+          comment="All the sweets are soooo lovely jubbly great price very fast delivery"
         />
       </div>
     </div>
