159c2fc163d0c95a2d57995f0d5f3d9f2f89626e
Maksim <maksim.bobrov@nuacom.net>
2023-04-19 12:20:46 +0300

    some changes in package json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.scss';
 import Header from './components/header/header';
