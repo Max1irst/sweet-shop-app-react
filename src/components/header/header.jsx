eaedeba725700b759e5a49d1829e9bfe69f22c4f
Maksim <maksim.bobrov@nuacom.net>
2023-12-16 10:44:45 +0200

    wip

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,78 +1,72 @@
 import { NavLink } from 'react-router-dom';
+import { useRef } from 'react';
 import { useSelector } from 'react-redux/es/hooks/useSelector';
 import './header.scss';
 import logo from '../../assets/images/logo.webp';
-import searchIcon from '../../assets/images/search-icon.png';
-import helpIcon from '../../assets/images/help-icon.png';
-import profileIcon from '../../assets/images/profile-icon.png';
 import basketIcon from '../../assets/images/basket-icon.png';
 function Header() {
+  const navRef = useRef()
+  const navHeight = useRef()
+  const huddleNavbar = () => {
+    navRef.current.classList.toggle('responsive-nav')
+    navHeight.current.classList.toggle('toggleHeight')
+  }
+
   const { cartTotalQuantity } = useSelector(state => state.cart)
   return (
     <div>
       <div className="header">
-        <div className="search">
-          <input placeholder="Search for your favorite sweets" />
-          <button className="search-btn">
-            <img src={searchIcon} alt="search"></img>
-          </button>
+        <div className='nav-btn'>
+          <button onClick={() => huddleNavbar()}><i className="bi bi-list bi-success"></i></button>
         </div>
         <NavLink to="/sweet-shop-app">
-          <img src={logo} alt="logo" />
+          <img className='logo' src={logo} alt="logo" />
         </NavLink>
         <div className="menu">
-          <NavLink to="/help">
-            <img id='getHelp' src={helpIcon} alt="help"></img>
-            Help
-          </NavLink>
-          <NavLink to="/account">
-            <img id='account' src={profileIcon} alt="account"></img>
-            Account
-          </NavLink>
           <NavLink to="/cart">
             <img id='cart' src={basketIcon} alt="cart" />
             <span className='counter'>{cartTotalQuantity}</span>
-            Basket
+            <p>Basket</p>
           </NavLink>
         </div>
       </div>
-      <div className="nav">
-        <ul>
-          <li>
+      <div ref={navHeight} className="nav">
+        <ul ref={navRef}>
+          <li onClick={() => huddleNavbar()}>
             <NavLink to="/shop_all_sweets" className={({ isActive }) => (isActive ? 'active' : '')}>
               Shop All Sweets
             </NavLink>
           </li>
-          <li>
+          <li onClick={() => huddleNavbar()}>
             <NavLink to="/shop_by_brand" className={({ isActive }) => (isActive ? 'active' : '')}>
               Shop By Brand
             </NavLink>
           </li>
-          <li>
+          <li onClick={() => huddleNavbar()}>
             <NavLink to="/sweets_by_color" className={({ isActive }) => (isActive ? 'active' : '')}>
               Sweets By Color
             </NavLink>
           </li>
-          <li>
+          <li onClick={() => huddleNavbar()}>
             <NavLink
               to="/sweets_by_flavour"
               className={({ isActive }) => (isActive ? 'active' : '')}>
               Sweets By Flavour
             </NavLink>
           </li>
-          <li>
+          <li onClick={() => huddleNavbar()}>
             <NavLink
               to="/sweets_by_occasion"
               className={({ isActive }) => (isActive ? 'active' : '')}>
               Sweets By Occasion
             </NavLink>
           </li>
-          <li>
+          <li onClick={() => huddleNavbar()}>
             <NavLink to="/shop_biscuits" className={({ isActive }) => (isActive ? 'active' : '')}>
               Shop Biscuits
             </NavLink>
           </li>
-          <li>
+          <li onClick={() => huddleNavbar()}>
             <NavLink to="/shop_crisps" className={({ isActive }) => (isActive ? 'active' : '')}>
               Shop Crisps
             </NavLink>
